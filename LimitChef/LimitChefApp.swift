bdfa3838f1abd6477aa46b5a62b190cec77fd179
NigelMrks <nigel.marks.flbk@gmail.com>
2023-06-22 10:32:38 +0200

    Initial Commit

---
+++ LimitChef/LimitChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimitChefApp.swift
+//  LimitChef
+//
+//  Created by Nigel Marks on 22.06.23.
+//
+
+import SwiftUI
+
+@main
+struct LimitChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
